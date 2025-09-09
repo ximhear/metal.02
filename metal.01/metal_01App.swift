88f4c88cb8341d6aeea4efb1a086cd6d6f22e2b2
C.H Lee <ximhear@gmail.com>
2025-09-10 00:58:41 +0900

    Initial Commit

---
+++ metal.01/metal_01App.swift
@@ -0,0 +1,17 @@
+//
+//  metal_01App.swift
+//  metal.01
+//
+//  Created by gzonelee on 9/10/25.
+//
+
+import SwiftUI
+
+@main
+struct metal_01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
